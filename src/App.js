73aa8e34b9ec1591d91938dfefc177b35ff53e00
mario piccinelli <mario.piccinelli@gmail.com>
2023-03-02 22:34:32 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import { Container, Typography, TextField, Box, FormControl } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import MuiAccordion from '@mui/material/Accordion';
+import MuiAccordionDetails from '@mui/material/AccordionDetails';
+import MuiAccordionSummary from '@mui/material/AccordionSummary';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import { tcodes } from './data';
 
 function App() {
+
+  const [expanded, setExpanded] = React.useState(false);
+  const [search, setSearch] = React.useState('');
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  let theme = createTheme({
+    typography: {
+      fontSize: 11,
+      fontFamily: "Segoe UI"
+    }
+  });
+
+  const Accordion = styled((props) => (
+    <MuiAccordion disableGutters elevation={0} square {...props} />
+  ))(({ theme }) => ({
+    border: `1px solid ${theme.palette.divider}`,
+    '&:not(:last-child)': {
+      borderBottom: 0,
+    },
+    '&:before': {
+      display: 'none',
+    },
+  }));
+
+  const AccordionSummary = styled((props) => (
+    <MuiAccordionSummary
+      // expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+      {...props}
+    />
+  ))(({ theme }) => ({
+    backgroundColor:
+      theme.palette.mode === 'dark'
+        ? 'rgba(255, 255, 255, .05)'
+        : 'rgba(0, 0, 0, .03)',
+    flexDirection: 'row-reverse',
+    '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+      transform: 'rotate(90deg)',
+    },
+    '& .MuiAccordionSummary-content': {
+      marginLeft: theme.spacing(0),
+    },
+  }));
+
+  const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+    padding: theme.spacing(1),
+    borderTop: '1px solid rgba(0, 0, 0, .125)',
+  }));
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <ThemeProvider theme={theme}>
+
+        <Container disableGutters maxWidth={false}>
+
+          <FormControl sx={{ m: 1, width:"-webkit-fill-available" }}>
+            <TextField id="outlined-basic" label="Ricerca TCODE" variant="outlined" onChange={e => setSearch(e.target.value)} />
+          </FormControl>
+
+          {
+            tcodes
+              .filter(item => item.keywords.toLowerCase().includes(search.toLowerCase()))
+              .map(item => (
+                <Accordion expanded={expanded === item.code} onChange={handleChange(item.code)} key={item.code}>
+                  <AccordionSummary>
+                    <Typography sx={{ fontWeight: 'bold' }}>{item.code}</Typography>
+                    <Typography sx={{ ml: 1 }}>{item.descr}</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography>{item.descr}</Typography>
+                    <Typography sx={{ fontStyle: 'italic' }}>Keywords: {item.keywords}</Typography>
+                  </AccordionDetails>
+                </Accordion>
+              ))
+          }
+
+        </Container>
+
+      </ThemeProvider>
     </div>
   );
 }
---
+++ webpack.dev.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+var InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+var publicUrl = '';
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  module: {
+	rules: [
+		{
+			test: /\.js|jsx$/,
+			exclude: /node_modules/,
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: [
+						'@babel/preset-env',
+						['@babel/preset-react', {"runtime":"automatic"}]
+					],
+				}
+			}
+		},
+		{
+			test: /\.css$/i,
+			use: [MiniCssExtractPlugin.loader, "css-loader"]
+		},
+		{
+			test: /\.svg$/i,
+			use: [
+				{
+					loader: 'svg-url-loader',
+					options: {
+						limit: 10000,
+					},
+				},
+			]
+		}
+	]
+  },
+  plugins: [
+	new HtmlWebpackPlugin({
+		inject: true,
+		template: './public/index.html',
+		filename: 'popup.html',
+	}),
+    new InterpolateHtmlPlugin(HtmlWebpackPlugin, {
+		PUBLIC_URL: publicUrl,
+	}),
+	new MiniCssExtractPlugin(),
+	new CopyPlugin({
+		patterns: [
+			{
+				from: 'public',
+			}
+		]
+	})
+  ]
+};
